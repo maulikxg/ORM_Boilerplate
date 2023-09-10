cdc0ca836585538b892d9053db7d96269425dfa0
maulik <maulikgoswami7021@gmail.com>
2023-09-10 17:48:22 +0530

    logger winston

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { ServerConfig } = require("./config");
+const { ServerConfig ,Logger } = require("./config");
 const apiRoutes = require("./routes");
 
 const app = express();
@@ -8,4 +8,5 @@ app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.portport, () => {
   console.log(`App listening on port ${ServerConfig.port}`);
+  Logger.info("Successfully started server" , "" , {})
 });
